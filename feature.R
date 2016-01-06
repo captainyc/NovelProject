ada6a08c834979fbd5a29f9a42fd5364bf5055f8
Yuancheng Zhu <yuancheng.zhu@gmail.com>
2016-01-05 23:24:39 -0600

    add cumsum & normalizaion features, other minor changes

--- feature.R
+++ feature.R
@@ -1,34 +1,28 @@
-feature_ttr <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL, normalize=FALSE, blocksize=1, replicate=100) {
+feature_ttr <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL, normalize=FALSE, cumsum=FALSE, replicate=100) {
 	chunks = chunk(text, nchunks, chunksize, remove)
 	TTR = sapply(1:length(chunks),function(i) length(unique(chunks[[i]]))/length(chunks[[i]]))
-	features = c(mean(TTR),sd(TTR))
+	features = data.frame(mean=mean(TTR),sd=sd(TTR))
 	if (normalize) {
-		TTR_perm = numeric(replicate)
-		for (j in 1:replicate) {
-			chunks_temp = chunk(text, nchunks, chunksize, randomize=TRUE, blocksize=blocksize)
-			TTR_perm[j] = mean(sapply(1:length(chunks_temp),function(i) length(unique(chunks_temp[[i]]))/length(chunks_temp[[i]])))
-		}
-		features = c(features,(mean(TTR)-mean(TTR_perm))/sd(TTR_perm))
+		TTR_norm = mean(sapply(1:replicate,function(i) length(unique(sample(text,length(chunks[[1]]))))/length(chunks[[1]])))
+		features$norm = mean(TTR)-TTR_norm
 	}
+	if (cumsum) features$cumsum = cumsum_test(TTR,replicate=replicate)
 	return(features)
 }
 
-feature_entropy <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL, normalize=FALSE, blocksize=1, replicate=100) {
+feature_entropy <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL, normalize=FALSE, cumsum=FALSE, replicate=100) {
 	chunks = chunk(text, nchunks, chunksize, remove)
 	entropy = sapply(1:length(chunks),function(i) {temp = table(chunks[[i]])/length(chunks[[i]]);sum(-temp*log(temp))} )
-	features = c(mean(entropy),sd(entropy))
+	features = data.frame(mean=mean(entropy),sd=sd(entropy))
 	if (normalize) {
-		entropy_perm = numeric(replicate)
-		for (j in 1:replicate) {
-			chunks_temp = chunk(text, nchunks, chunksize, randomize=TRUE, blocksize=blocksize)
-			entropy_perm[j] = mean(sapply(1:length(chunks_temp),function(i) {temp = table(chunks_temp[[i]])/length(chunks_temp[[i]]);sum(-temp*log(temp))} ))
-		}
-		features = c(features,(mean(entropy)-mean(entropy_perm))/sd(entropy_perm))
+		entropy_norm = mean(sapply(1:replicate, function(i) { temp = table(sample(text,length(chunks[[1]])))/length(chunks[[1]]); sum(-temp*log(temp)) }))
+		features$norm = (mean(entropy)-entropy_norm)
 	}
+	if (cumsum) features$cumsum = cumsum_test(entropy,replicate=replicate)
 	return(features)
 }
 
-feature_con_ent <- function(text, nchunks=NULL, chunksize=NULL) {
+feature_con_ent <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL, cumsum=FALSE, replicate=100) {
 	chunks = chunk(text, nchunks, chunksize, remove)
 	conditional_entropy = numeric(length(chunks))
 	eps = 1e-10
@@ -36,15 +30,19 @@ feature_con_ent <- function(text, nchunks=NULL, chunksize=NULL) {
 		pair_count = table(chunks[[i]][-length(chunks[[i]])],chunks[[i]][-1])
 		conditional_entropy[i] = -sum(log(pair_count/rowSums(pair_count)+eps)*(pair_count/sum(pair_count)))
 	}
-	return(c(mean(conditional_entropy),sd(conditional_entropy)))
+	features = data.frame(mean=mean(conditional_entropy),sd=sd(conditional_entropy))
+	if (cumsum) features$cumsum = cumsum_test(conditional_entropy,replicate=replicate)
+	return(features)
 }
 
-feature_dist_to_global <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL) {
+feature_dist_to_global <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL, cumsum=FALSE, replicate=100) {
 	chunks = chunk(text, nchunks, chunksize, remove)
 	global_vec = table(text)/length(text)
 	KL = sapply(1:length(chunks),function(i) { freq=table(chunks[[i]])/length(chunks[[i]]); return(kl_div(freq,global_vec[names(freq)])) } )
 	COS = sapply(1:length(chunks),function(i) { freq=table(chunks[[i]])/length(chunks[[i]]); return(cos_dist(freq,global_vec[names(freq)]))} )
-	return(c(mean(KL),sd(KL),mean(COS),sd(COS)))
+	features = data.frame(kl_mean=mean(KL),kl_sd=sd(KL),cos_mean=mean(COS),cos_sd=sd(COS))
+	if (cumsum) features$kl_cumsum = cumsum_test(KL,replicate=replicate); features$cos_cumsum = cumsum_test(COS,replicate=replicate)
+	return(features)
 }
 
 feature_cross_half_score <- function(text, nchunks=NULL, chunksize=NULL, remove=NULL) {
