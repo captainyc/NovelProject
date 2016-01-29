0750631e82cd6ae3d3c339f63b937a4626c4e0ab
Yuancheng Zhu <yuancheng.zhu@gmail.com>
2016-01-29 09:49:03 -0600

    update for the latest corpus

--- test.R
+++ test.R
@@ -2,8 +2,9 @@ library(glmnet)
 source("./utils.R")
 source("./feature.R")
 
-meta_ch = read.csv("./test/CmetricsTest.csv")
-meta_jp = read.csv("./test/JmetricsTest.csv")
+#meta_ch = read.csv("./test/CmetricsTest.csv")
+meta_ch = read.csv("./test/CmetricsFinal.csv")
+meta_jp = read.csv("./test/JmetricsFinal.csv")
 feature_name = c("ttr_mean","ttr_sd","ttr_norm","ttr_cumsum",
                  "ent_mean","ent_sd","ent_norm","ent_cumsum",
                  "con_ent_mean","con_ent_sd","con_ent_cumsum",
@@ -12,7 +13,7 @@ feature_name = c("ttr_mean","ttr_sd","ttr_norm","ttr_cumsum",
                  "dist_cos_mean","dist_cos_sd","dist_cos_cumsum",
                  "cross_half_l2","cross_half_tfidf",
                  "cross_half_l2_nostopword","cross_half_tfidf_nostopword",
-                 "thought","pronoun_first","pronoun_third",
+                 "thought2","pronoun_first","pronoun_third",
                  "period","punct","stopword")
 meta_ch = cbind(meta_ch,matrix(0,nrow=nrow(meta_ch),ncol=length(feature_name),dimnames=list(NULL,feature_name)))
 meta_jp = cbind(meta_jp,matrix(0,nrow=nrow(meta_jp),ncol=length(feature_name),dimnames=list(NULL,feature_name)))
@@ -31,7 +32,7 @@ cat("Generating features for the Chinese corpus...\n")
 pb = txtProgressBar(style=3)
 record_ch = data.frame(ttr=NULL,ent=NULL,con_ent=NULL)
 for (i in 1:nrow(meta_ch)) {
-	text = scan(paste0("~/github/NovelProject/data/chinese/",meta_ch$file_id_name[i]), what="character", sep=" ", quote="", quiet=TRUE)
+	text = scan(paste0("~/github/NovelProject/data/chinese/",meta_ch$file_text_name[i]), what="character", sep=" ", quote="", quiet=TRUE)
 	text = text[text!=""]
 	meta_ch[i,c("ttr_mean","ttr_sd","ttr_norm","ttr_cumsum")] = feature_ttr(text, chunksize=chunksize, remove=punct$ch, normalize=TRUE, cumsum=TRUE, replicate=500)
 	meta_ch[i,c("ent_mean","ent_sd","ent_norm","ent_cumsum")] = feature_entropy(text, chunksize=chunksize, remove=punct$ch, normalize=TRUE, cumsum=TRUE, replicate=500)
@@ -40,7 +41,7 @@ for (i in 1:nrow(meta_ch)) {
 	meta_ch[i,c("dist_kl_mean","dist_kl_sd","dist_cos_mean","dist_cos_sd","dist_kl_cumsum","dist_cos_cumsum")] = feature_dist_to_global(text, nchunks=20, remove=punct$ch)
 	meta_ch[i,c("cross_half_l2","cross_half_tfidf")] = feature_cross_half_score(text, nchunks=20, remove=punct$ch)
 	meta_ch[i,c("cross_half_l2_nostopword","cross_half_tfidf_nostopword")] = feature_cross_half_score(text,nchunks=20,remove=c(stopword$ch,punct$ch))
-	meta_ch[i,"thought"] = mean(text %in% thought$ch)
+	meta_ch[i,"thought2"] = mean(text %in% thought$ch)
 	meta_ch[i,"pronoun_first"] = mean(text=="我")
 	meta_ch[i,"pronoun_third"] = mean(text=="他"|text=="她")
 	meta_ch[i,"period"] = mean(text=="。")
@@ -54,12 +55,12 @@ cat("Recording chunkwise measurements...\n")
 pb = txtProgressBar(style=3)
 record_ch = data.frame(file_id=NULL,chunk_no=NULL,ttr=NULL,ent=NULL,con_ent=NULL)
 for (i in 1:nrow(meta_ch)) {
-	text = scan(paste0("~/github/NovelProject/data/chinese/",meta_ch$file_id_name[i]), what="character", sep=" ", quote="", quiet=TRUE)
+	text = scan(paste0("~/github/NovelProject/data/chinese/",meta_ch$file_text_name[i]), what="character", sep=" ", quote="", quiet=TRUE)
 	text = text[text!=""]
 	ttr = feature_ttr(text, chunksize=chunksize, remove=punct$ch, return.value=TRUE)
 	ent = feature_entropy(text, chunksize=chunksize, remove=punct$ch, return.value=TRUE)
 	con_ent = feature_con_ent_new(text, chunksize=chunksize, remove=punct$ch, return.value=TRUE)
-	record_ch = rbind(record_ch,data.frame(file_id=rep(meta_ch$file_id_name[i],length(ttr)),chunk_no=1:length(ttr),ttr=ttr,ent=ent,con_ent=con_ent))
+	record_ch = rbind(record_ch,data.frame(file_id=rep(meta_ch$file_text_name[i],length(ttr)),chunk_no=1:length(ttr),ttr=ttr,ent=ent,con_ent=con_ent))
 	setTxtProgressBar(pb,i/nrow(meta_ch))
 }
 close(pb)
@@ -76,7 +77,7 @@ for (i in 1:nrow(meta_jp)) {
 	meta_jp[i,c("dist_kl_mean","dist_kl_sd","dist_cos_mean","dist_cos_sd","dist_kl_cumsum","dist_cos_cumsum")] = feature_dist_to_global(text, nchunks=20, remove=punct$jp)
 	meta_jp[i,c("cross_half_l2","cross_half_tfidf")] = feature_cross_half_score(text, nchunks=20, remove=punct$jp)
 	meta_jp[i,c("cross_half_l2_nostopword","cross_half_tfidf_nostopword")] = feature_cross_half_score(text,nchunks=20,remove=c(stopword$jp,punct$jp))
-	meta_jp[i,"thought"] = mean(text %in% thought$jp)
+	meta_jp[i,"thought2"] = mean(text %in% thought$jp)
 	#meta_jp[i,"pronoun_first"] = mean(text=="我")
 	#meta_jp[i,"pronoun_third"] = mean(text=="他"|text=="她")
 	meta_jp[i,"period"] = mean(text=="。")
@@ -135,7 +136,7 @@ for (i in 1:B) {
 	sr = sample(which(meta_ch$genre=="SR"))
 	pop = sample(which(meta_ch$genre=="Pop"))
 	
-	ptrain = 9/10
+	ptrain = 4/5
 	train = meta_ch[c(romantic[1:floor(length(romantic)*ptrain)],sr[1:floor(length(sr)*ptrain)],pop[1:floor(length(pop)*ptrain)]),]
 	test = meta_ch[c(romantic[(floor(length(romantic)*ptrain)+1):length(romantic)],sr[(floor(length(sr)*ptrain)+1):length(sr)],pop[(floor(length(pop)*ptrain)+1):length(pop)]),]
 	features = c("kl_score","ttr_mean","ent_mean","con_ent_mean","thought","pronoun_first","pronoun_third")
